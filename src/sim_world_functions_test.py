0d87b8d013a3cfc25f65022eb6092489519a9bf8
iamgiddyaboutgit <justinwitherlypatterson@gmail.com>
2023-12-06 17:25:27 -0500

    Use this for testing.

---
+++ src/sim_world_functions_test.py
@@ -0,0 +1,26 @@
+#!/usr/bin/env python3
+"""Tests functions in file sim_world_functions.py
+"""
+
+import sim_world_functions as swf
+
+import numpy as np
+
+# print(
+#     swf.stoch_pro_lin(
+#         x_0=0,
+#         t_max=50,
+#         slope=-1,
+#         alpha=0.1,
+#         sigma_errors=1/51,
+#         x_min=-10,
+#         x_max=40
+#     )
+# )
+
+# print(
+#     swf.weight_0_more(
+#         x=np.linspace(0, 1, 50),
+#         b=-6
+#     )
+# )
